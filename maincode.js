bcaf7e31136bee8593f8f61c10aee54b96728cc1
Whilegames <128980648+Whilegames@users.noreply.github.com>
2023-03-26 16:18:02 +0200

    Update maincode.js

---
+++ maincode.js
@@ -0,0 +1,8 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-P1M2CXKN3S"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-P1M2CXKN3S');
+</script>
