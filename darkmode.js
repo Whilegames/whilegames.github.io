9cb7c91f24c52f37811cd342d1040ac8bfbaf51e
Whilegames <128980648+Whilegames@users.noreply.github.com>
2023-03-30 15:43:26 +0200

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -1,12 +1,17 @@
 const toggleSwitch = document.querySelector('#dark-mode-toggle');
 const body = document.querySelector('body');
+const gameDescription = document.querySelector('#game-description');
 
 toggleSwitch.addEventListener('change', function() {
   if (this.checked) {
     body.classList.add('dark-mode');
     body.classList.remove('light-mode');
+    gameDescription.classList.add('game-description-dark');
+    gameDescription.classList.remove('game-description-light');
   } else {
     body.classList.add('light-mode');
     body.classList.remove('dark-mode');
+    gameDescription.classList.add('game-description-light');
+    gameDescription.classList.remove('game-description-dark');
   }    
 });
